52a953abca16cb7af4cd2e1750bb9cfe822ec94d
athyy2311 <9fj4dh642n@privaterelay.appleid.com>
2026-06-16 16:48:18 +0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("IMG_9660.jpeg")
 # Tiêu đề ứng dụng 
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_ Đề tài 2_ Nguyễn Anh Thy")
 
 # Nhập dữ liệu
 C = st.number_input(
